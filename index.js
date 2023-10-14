5d7d2147dbb66cc18194c8262bf713967e972edb
Dvwhite81 <Dvwhite1981@gmail.com>
2023-10-14 19:32:41 -0400

    test

--- index.js
+++ index.js
@@ -1,126 +1,140 @@
-require('dotenv').config()
+require("dotenv").config();
+
 const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
 
-const Person = require('./models/person')
-
-app.use(express.static('dist'))
+app.use(cors());
 app.use(express.json());
+app.use(express.static("build"));
 
-const requestLogger = (req, res, next) => {
-	console.log('Method:', req.method);
-	console.log('Path:', req.path)
-	console.log('Body:', req.body);
-	console.log('---');
-	next()
-}
-app.use(requestLogger)
-
-morgan.token("post", (req) => {
-  if (req.method === "POST") {
-    return JSON.stringify(req.body);
-  } else {
-    return "";
-  }
+morgan.token("post", (request, _response) => {
+  "use strict";
+  if (request.method === "POST") return JSON.stringify(request.body);
+  else return "";
 });
 
 morgan.format(
   "postFormat",
-  ":method :url :status :res[content-length] - :response-time ms :post"
+  ":method :url :status :res[content-length] - :response-time ms | :post"
 );
+
 app.use(morgan("postFormat"));
-app.use(cors())
 
-app.get("/info", async (req, res) => {
-  const persons = await Person.find({})
-  const currentDate = new Date().toDateString();
-  const currentTime = new Date().toTimeString();
-  const info = `
-		<p>Phonebook has info for ${persons.length} people</p>
-		<p>${currentDate} ${currentTime}</p>
-	`;
-  res.send(info);
+const Person = require("./models/person");
+
+app.get("/info", (_request, response, next) => {
+  "use strict";
+  Person.countDocuments({})
+    .then((results) => {
+      response.send(`
+    <p>Phonebook has info for ${results} people</p>
+    <p>${new Date()}</p>
+    `);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons", (req, res) => {
-  Person.find({}).then(persons => {
-		res.json(persons)
-	})
+app.get("/api/persons", (_request, response, next) => {
+  "use strict";
+  Person.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (req, res, next) => {
-  Person.findById(req.params.id)
-		.then(person => {
-			if (person) {
-				res.json(person)
-			} else {
-				res.status(404).end()
-			}
-		})
-		.catch(error => next(error))
+app.post("/api/persons", (request, response, next) => {
+  "use strict";
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number is missing",
+    });
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (req, res) => {
-  const body = req.body;
-
-  if (body.name === undefined || body.number === undefined) {
-		return res.status(400).json({ error: 'name or number missing' })
-	}
-
-	const person = new Person({
-		name: body.name,
-		number: body.number
-	})
-
-	person.save().then(savedPerson => {
-		res.json(savedPerson)
-	})
+app.get("/api/persons/:id", (request, response, next) => {
+  "use strict";
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete('/api/persons/:id', (req, res, next) => {
-	Person.findByIdAndRemove(req.params.id)
-		.then(result => {
-			res.status(204).end()
-		})
-		.catch(error => next(error))
-})
+app.delete("/api/persons/:id", (request, response, next) => {
+  "use strict";
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
-app.put('/api/persons/:id', (req, res, next) => {
-	const { name, number } = req.body
+app.put("/api/persons/:id", (request, response, next) => {
+  "use strict";
+  const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
 
-	Person.findByIdAndUpdate(
-		req.params.id,
-		{ name, number },
-		{ new: true, runValidators: true, context: 'query'}
-	)
-		.then(updatedPerson => {
-			res.json(updatedPerson)
-		})
-		.catch(error => next(error))
-})
+const unknownEndpoint = (_request, response) => {
+  "use strict";
+  response.status(404).send({error: "unknown endpoint"});
+};
 
-const unknownEndpoint = (req, res) => {
-	res.status(404).send({ error: 'unknown endpoint' })
-}
+app.use(unknownEndpoint);
 
-app.use(unknownEndpoint)
+const errorHandler = (error, _request, response, next) => {
+  "use strict";
+  console.log(error.name);
+  console.error(error.message);
 
-const errorHandler = (error, req, res, next) => {
-	console.error(error.message)
+  if (error.name === "CastError") {
+    return response.status(400).send({error: "Malformatted"});
+  }
 
-	if (error.name === 'CastError') {
-		return res.status(400).send({ error: 'malformatted id' })
-	} else if (error.name === 'ValidationError') {
-		return res.status(400).json({ error: error.message })
-	}
-	next(error)
-}
+  if (error.name === "ValidationError") {
+    return response.status(400).send({error: error.message});
+  }
 
-app.use(errorHandler)
+  next(error);
+};
 
-const PORT = process.env.PORT
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  "use strict";
+  console.log(`Server running on ${PORT}`);
 });
