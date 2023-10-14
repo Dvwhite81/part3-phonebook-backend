75470d6198dd9960cc671426656d184562928cc4
Dvwhite81 <Dvwhite1981@gmail.com>
2023-10-14 14:05:55 -0400

    testing 3.15 - 3.18

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const Person = require('./models/person')
 app.use(express.static('dist'))
 app.use(express.json());
 
+const requestLogger = (req, res, next) => {
+	console.log('Method:', req.method);
+	console.log('Path:', req.path)
+	console.log('Body:', req.body);
+	console.log('---');
+	next()
+}
+app.use(requestLogger)
+
 morgan.token("post", (req) => {
   if (req.method === "POST") {
     return JSON.stringify(req.body);
@@ -41,10 +50,16 @@ app.get("/api/persons", (req, res) => {
 	})
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  Person.findById(req.params.id).then(person => {
-		res.json(person)
-	})
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+		.then(person => {
+			if (person) {
+				res.json(person)
+			} else {
+				res.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 });
 
 app.post("/api/persons", (req, res) => {
@@ -63,6 +78,46 @@ app.post("/api/persons", (req, res) => {
 		res.json(savedPerson)
 	})
 });
+
+app.delete('/api/persons/:id', (req, res, next) => {
+	Person.findByIdAndRemove(req.params.id)
+		.then(result => {
+			res.status(204).end()
+		})
+		.catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+	const body = req.body
+
+	const person = {
+		name: body.name,
+		number: body.number
+	}
+
+	Person.findByIdAndUpdate(req.params.id, person, { new: true })
+		.then(updatedPerson => {
+			res.json(updatedPerson)
+		})
+		.catch(error => next(error))
+})
+
+const unknownEndpoint = (req, res) => {
+	res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return res.status(400).send({ error: 'malformatted id' })
+	}
+	next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
